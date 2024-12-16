65d5c752985b9d3165effc017e6e69c0ac21dcc0
tsion22girma <tsion22girma@gmail.com>
2024-12-16 12:08:50 -0600

    add action

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -1,2 +1,58 @@
 import {z} from 'zod'
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 
+const InvoiceFormSchema = z.object({
+    id: z.string(),
+    customerId: z.string({
+        invalid_type_error: 'please select a customer'
+    }),
+    amount: z.coerce.number()
+    .gt(0, {message: 'please enter amount geater than $0'}),
+    date: z.string(),
+    status: z.enum(['pending', 'paid'], {invalid_type_error: 'please select an invoice status'})
+})
+
+const CreateInvoice = InvoiceFormSchema.omit({id: true, date: true})
+
+export type InvoiceState = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: InvoiceState, formData: FormData) {
+    const validateFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    })
+
+    if(!validateFields.success) {
+        return {
+            errors: validateFields.error.flatten().fieldErrors,
+            message: 'missing fields. faild to create invoice'
+        }
+    }
+
+    const {customerId, amount, status} = validateFields.data;
+    const amountInCents = amount * 100;
+    const id = '7c80c350-e80d-4a0a-b849-a80958c24008';
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`insert into invoice (customer_id, amount, status, date) values 
+        (${customerId}, ${amountInCents}, ${status}. ${date})`;
+    } catch (error) {
+        return {
+            message: 'Database Error: failed to create invoice.'
+        }
+    }
+
+    revalidatePath('/invoices');
+    redirect('/invoices');
+}
--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,6 +1,16 @@
+import { string } from "zod";
+
 export type Customer = {
     id: string;
     name: string;
     email: string;
     image_url: string;
+}
+
+export type Invoice = {
+    id: string;
+    customerId: string;
+    amount: number;
+    date: string;
+    status: 'pending'| 'paid';
 }
