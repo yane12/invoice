56393fae7e025898d920cb5ecf89bb76793cc9da
tsion22girma <tsion22girma@gmail.com>
2024-12-18 14:35:58 -0600

    create customer pagination

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { fetchCustomer } from '../lib/customer/data'
 import Link from 'next/link'
 import Search from '../ui/customer/search'
+import Pagination from '../ui/customer/pagination'
 
 export default async function Page() {
   const customers = await fetchCustomer()
+  const totalPages = customers.length
   return (
     <div>
       <h1 className='mb-4'>Customers</h1>
@@ -15,6 +17,8 @@ export default async function Page() {
           <li key={customer.id}>{customer.name}</li>
         ))}
       </ul>
+
+      <Pagination totalPages={totalPages}/>
     </div>
   )
 }
--- src/app/ui/customer/pagination.tsx
+++ src/app/ui/customer/pagination.tsx
@@ -1,7 +1,45 @@
+'use client'
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/16/solid'
+import Link from 'next/link'
+import { usePathname, useSearchParams } from 'next/navigation'
 import React from 'react'
 
-export default function Pagination() {
-  return (
-    <div>Pagination</div>
-  )
+export default function Pagination({ totalPages }: { totalPages: number }) {
+    const searchParams = useSearchParams()
+    const pathname = usePathname()
+    const currentPage = Number(searchParams.get('page')) | 1;
+
+    const createPageUrl = (pageNumber: string | number) => {
+        const params = new URLSearchParams(searchParams)
+        params.set('page', pageNumber.toString())
+        return `${pathname}?${params.toString()}`;
+    }
+
+    return (
+        <div>
+            <PaginationArrow direction='left' href={createPageUrl(currentPage - 1)} isDisabled={currentPage <= 1} />
+            <PaginationArrow direction='right' href={createPageUrl(currentPage + 1)} isDisabled={currentPage >= totalPages}  />
+        </div>
+    )
+}
+
+function PaginationArrow(
+    {
+        direction,
+        href,
+        isDisabled
+    }:
+        {
+            direction: string,
+            href: string,
+            isDisabled: boolean
+        }
+) {
+    const icon = direction === 'left' ? (<ArrowLeftIcon className='w-4'></ArrowLeftIcon>) :
+        (<ArrowRightIcon className='w-4'></ArrowRightIcon>)
+    return isDisabled ? (
+        <div>{icon}</div>) :
+        (
+            <Link href={href}>{icon}</Link>
+        )
 }
