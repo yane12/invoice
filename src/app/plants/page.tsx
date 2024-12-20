361ff24ed06c79e8658452ced73552949194ad5a
tsion22girma <tsion22girma@gmail.com>
2024-12-20 10:34:09 -0600

    create plants ui

---
+++ src/app/plants/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div>
+            <Link href='/plants/create'>New Plant</Link>
+            Page
+        </div>
+    )
+}
